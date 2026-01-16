80de3dae6b16acd59c95e69ca584c8b03e1d56d7
Cotteux <58077544+cotteux@users.noreply.github.com>
2026-01-16 10:44:27 -0500

    translate in French

--- example.py
+++ example.py
@@ -1,19 +1,41 @@
 from pyfrotz import Frotz
 from pyfrotz.parsers import advent_intro_parser, planetfall_intro_parser
+import time
 # load your game file
-data = '/home/miro/PycharmProjects/OpenJarbas/pyFrotz/Advent.z5'
+data = 'games/zork1.z5'
 #data = "/home/miro/PycharmProjects/GameSkills/ovos-skill-planet-fall-game/res/planetfall.z5"
-game = Frotz(data)
+game = Frotz(data,"/usr/games/dfrotz")
 
 
 # use it inside code
-#game_intro = game.get_intro()
-#room, description = game.do_command("\n")
+#game_intro = game.parse_intro()
+#print(game_intro)
+#description  = game.do_command("north")
+
+#print (room)
+#print (description)
+#time.sleep(0.5)
+#des = game.restore('SAVE/zork1.qzl')
+#print (des)
+#time.sleep(0.5)
+#description  = game.do_command("restore")
+#print (description)
+#time.sleep(0.4)
+#description  = game.do_command("zorktest.qzl")
+#print (description)
+#time.sleep(0.4)
+#description  = game.do_command("north")
+
+#print (description)
+#time.sleep(0.4)
+#description  = game.save("SAVE/zork1.qzl")
+#print (description)
 
 
-
+#description  = game.do_command("zorktest.qzl\n")
+#print (description)
 # or play in the cli
-game = Frotz(data,
-             intro_parser=advent_intro_parser)
+
+             
 game.play_loop()
 
